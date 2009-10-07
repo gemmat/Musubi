057e9fb410bda3f2256bd5d575ccbb7321cc28e6
Teruaki Gemma <teruakigemma@gmail.com>
2009-10-07 17:24:32 +0900

    Say good bye to the filterBrowsersByURI.

--- chrome/content/browser/30-xmppweb.js
+++ chrome/content/browser/30-xmppweb.js
@@ -1,26 +1,26 @@
-const EXPORT = ["filterBrowsersByURI", "makeChannel"];
+const EXPORT = ["filterBrowsers", "makeChannel"];
 
-// export filterBrowsersByURI just for the debug.
-function filterBrowsersByURI(aAccount, aSendto, aResource, aHref) {
-  var res0 = [], res1 = [];
+// export filterBrowsers just for the debug.
+function filterBrowsers(aFrom, aTo) {
+  var res = [];
+  if (!gBrowser) return res;
   for (var i = 0, len = gBrowser.browsers.length; i < len; i++) {
     var b = gBrowser.getBrowserAtIndex(i);
     var o = parseURI(b.currentURI.spec);
     if (!o) continue;
-    if (o.account  == aAccount &&
-        o.sendto   == aSendto &&
-        (!aHref || o.href == aHref)) {
-      if (o.resource) {
-        if (o.resource == aResource) {
-          res0.push(b);
-        }
-      } else {
-        res1.push(b);
+    var auth = parseJID(o.auth);
+    var path = parseJID(o.path);
+    if (!auth || !path) continue;
+    if (( aTo.resource && auth.fulljid == aTo.fulljid) ||
+        (!aTo.resource && auth.barejid == aTo.barejid)) {
+      if (path.barejid == aFrom.barejid) {
+      //if (( aFrom.resource && path.fulljid == aFrom.fulljid) ||
+          //(!aFrom.resource && path.barejid == aFrom.barejid)) {
+        res.push(b);
       }
     }
   }
-  if (res0.length && res1.length) return res0;
-  return res0.concat(res1);
+  return res;
 }
 
 function makeChannel() {
@@ -35,9 +35,10 @@ function parseXMPP4MOZEvent(aObject) {
   var stanza = aObject.stanza;
   var from = parseJID(stanza.@from.toString());
   var to   = parseJID(stanza.@to.length() ? stanza.@to.toString() : aObject.account);
-  var nsOob = new Namespace("jabber:x:oob");
-  var url  = stanza.nsOob::x.nsOob::url.toString();
-  return [stanza, from, to, url];
+  print("stanza:" + stanza.toXMLString());
+  print("from:" + toJSON(from));
+  print("to:"   + toJSON(to));
+  return [stanza, from, to];
 }
 
 function appendStanzaToBrowsers(aBrowsers, aStanza) {
@@ -55,23 +56,9 @@ function appendStanzaToSidebar(aStanza) {
 
 //We call onMessage many times so we need to be aware of the performance.
 function onMessage(aObj) {
-  var [stanza, from, to, url] = parseXMPP4MOZEvent(aObj);
+  var [stanza, from, to] = parseXMPP4MOZEvent(aObj);
   if (from && to) {
-    if (appendStanzaToBrowsers(
-          filterBrowsersByURI(to.barejid, from.barejid, from.resource, url),
-          stanza)) return;
-    if (url) {
-      var newTab = gBrowser.getBrowserForTab(
-                     gBrowser.addTab(makeXmppURI(to.barejid, from.barejid, from.resource, "share", url)));
-      var appendOnload0 = function(e) {
-        newTab.contentDocument.addEventListener("load", appendOnload1, true);
-        newTab.removeEventListener("load", appendOnload0, true);
-      };
-      var appendOnload1 = function(e) {
-        appendE4XToXmppIn(newTab.contentDocument, stanza);
-        newTab.contentDocument.removeEventListener("load", appendOnload1, true);
-      };
-      newTab.addEventListener("load", appendOnload0, true);
+    if (appendStanzaToBrowsers(filterBrowsers(from, to), stanza)) {
       return;
     }
   }
@@ -79,21 +66,17 @@ function onMessage(aObj) {
 }
 
 function onPresence(aObj) {
-  var [stanza, from, to, _] = parseXMPP4MOZEvent(aObj);
+  var [stanza, from, to] = parseXMPP4MOZEvent(aObj);
   if (from && to) {
-    appendStanzaToBrowsers(
-      filterBrowsersByURI(to.barejid, from.barejid, from.resource),
-      stanza);
+    appendStanzaToBrowsers(filterBrowsers(from, to), stanza);
   }
   appendStanzaToSidebar(stanza);
 }
 
 function onIQ(aObj) {
-  var [stanza, from, to, _] = parseXMPP4MOZEvent(aObj);
+  var [stanza, from, to] = parseXMPP4MOZEvent(aObj);
   if (from && to) {
-    appendStanzaToBrowsers(
-      filterBrowsersByURI(to.barejid, from.barejid, from.resource),
-      stanza);
+    appendStanzaToBrowsers(filterBrowsers(from, to), stanza);
   }
   appendStanzaToSidebar(stanza);
 }
